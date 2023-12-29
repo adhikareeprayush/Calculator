ee5ae0979f1f5d7328cd9ef9fd906cbbeba237df
Prayush Adhikari <115662438+adhikareeprayush@users.noreply.github.com>
2023-12-29 18:09:25 +0545

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function toggleOption(option) {
+    const degOption = document.querySelector('.toggle-option:nth-child(1)');
+    const radOption = document.querySelector('.toggle-option:nth-child(2)');
+    const slider = document.querySelector('.toggle-slider');
+
+    if (option === 'deg') {
+        degOption.classList.add('active');
+        radOption.classList.remove('active');
+        slider.style.left = '0'; // Adjusted the left position
+    } else if (option === 'rad') {
+        degOption.classList.remove('active');
+        radOption.classList.add('active');
+        slider.style.left = '50%'; // Adjusted the left position
+    }
+}
